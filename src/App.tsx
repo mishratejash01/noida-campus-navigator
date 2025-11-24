cb620c8870d8cb901d1e6604df488c9de6351139
Tejash Mishra <mtejash07@gmail.com>
2025-11-24 16:39:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Colleges from "./pages/Colleges";
-import UniversityColleges from "./pages/UniversityColleges"; // Imported missing page
 import CollegeDetails from "./pages/CollegeDetails";
-import Resources from "./pages/Resources";
-import Internships from "./pages/Internships";
+import UniversityColleges from "./pages/UniversityColleges";
 import Events from "./pages/Events";
 import Housing from "./pages/Housing";
+import Resources from "./pages/Resources";
+import Internships from "./pages/Internships";
 import CampusPulse from "./pages/CampusPulse";
-import { Footer } from "./components/Footer"; 
+// New Imports
+import Marketplace from "./pages/Marketplace";
+import Survival from "./pages/Survival";
+import FunZone from "./pages/FunZone";
 
 const queryClient = new QueryClient();
 
@@ -23,24 +27,28 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="flex min-h-screen flex-col">
-          <div className="flex-1">
-            <Routes>
+        <div className="min-h-screen flex flex-col bg-slate-50/50">
+          <Navigation />
+          <div className="flex-grow pt-4">
+             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/colleges" element={<Colleges />} />
-              {/* Added Route for University-specific colleges */}
-              <Route path="/colleges/:universityId" element={<UniversityColleges />} />
-              <Route path="/college/:id" element={<CollegeDetails />} />
-              <Route path="/resources" element={<Resources />} />
-              <Route path="/internships" element={<Internships />} />
+              <Route path="/colleges/:id" element={<CollegeDetails />} />
+              <Route path="/university/:id" element={<UniversityColleges />} />
               <Route path="/events" element={<Events />} />
               <Route path="/housing" element={<Housing />} />
+              <Route path="/resources" element={<Resources />} />
+              <Route path="/internships" element={<Internships />} />
               <Route path="/pulse" element={<CampusPulse />} />
+              
+              {/* New Routes */}
+              <Route path="/marketplace" element={<Marketplace />} />
+              <Route path="/survival" element={<Survival />} />
+              <Route path="/fun" element={<FunZone />} />
+              
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
-          {/* Footer added here so it sits at the bottom of all pages */}
-          <Footer />
         </div>
       </BrowserRouter>
     </TooltipProvider>
